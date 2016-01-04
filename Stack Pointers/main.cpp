aca2c83eb9aa60311026e555b6e0434ff480b715
Timothy <eclisesolaris23@gmail.com>
2016-01-04 12:26:19 -0500

    Initial Commit

---
+++ Stack Pointers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Stack Pointers
+//
+//  Created by newuser on 1/4/16.
+//  Copyright © 2016 Stack_Pointer. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
